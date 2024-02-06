2df3a8ec121401e16f54519acfad3db6f99648e2
azizul-orvee <orvee16@gmail.com>
2024-02-07 04:51:40 +0600

    asdas

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,13 +4,10 @@ import Image from "next/image";
 import { useState } from "react";
 import Customers from "./Customers";
 
-
 const HomePage = () => {
-
-
   return (
     <>
-      <section className="bg-white-500 text-black h-screen">
+      <section className="bg-white-500 text-black h-screen mt-16">
         <div className="container mx-auto px-4 flex flex-col md:flex-row py-10">
           <div className="md:w-1/2 p-4 bg-gray-100 rounded-lg overflow-hidden shadow-xl transform transition-all hover:scale-105 hover:shadow-2xl">
             <Image
@@ -22,12 +19,15 @@ const HomePage = () => {
           </div>
           <div className="md:w-1/2 flex justify-center items-center text-center">
             <div>
-              <h1 className="text-4xl font-bold mt-8 text-gray-900">Mobile <span className="text-orange-600">Tyre</span>  Fitting</h1>
+              <h1 className="text-4xl font-bold mt-8 text-gray-900 font-sans">
+                Mobile <span className="text-orange-600">Tyre</span> Fitting
+              </h1>
               <p className="text-lg m-4 mx-6 text-gray-900">
-              Our mobile tire fitting service brings convenience to your doorstep.
-               We offer expert tire installation, balancing, and alignment at a location
-                of your choice. Say goodbye to the hassle of visiting a garage –
-                 we will ensure your tires are professionally fitted, keeping you safe on the road.
+                Our mobile tire fitting service brings convenience to your
+                doorstep. We offer expert tire installation, balancing, and
+                alignment at a location of your choice. Say goodbye to the
+                hassle of visiting a garage – we will ensure your tires are
+                professionally fitted, keeping you safe on the road.
               </p>
             </div>
           </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,34 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Navbar() {
   const [navbar, setNavbar] = useState(false);
+  const [scroll, setScroll] = useState(false);
+
+  // This function will be triggered on scroll
+  const changeBackground = () => {
+    if (window.scrollY >= 80) {
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeBackground);
+    return () => {
+      window.removeEventListener("scroll", changeBackground);
+    };
+  }, []);
   return (
     <div>
-      <nav className="w-full top-0 left-0 right-0 z-10 bg-gray-500">
+      <nav
+        className={`w-full top-0 left-0 right-0 z-10 transition-all duration-100 shadow-sm fixed ease-in-out ${
+          scroll ? "border-b-2 bg-white" : "bg-white"
+        }`}
+      >
         <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
           <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
@@ -79,25 +100,20 @@ function Navbar() {
                 navbar ? "p-12 md:p-0 block" : "hidden"
               }`}
             >
-              <ul className="h-screen md:h-auto items-center justify-center md:flex ">
-                <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+              <ul className="md:h-auto items-center justify-center md:flex">
+                <li className="pb-6 text-xl text-orange-600 py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
                   <Link href="#about" onClick={() => setNavbar(!navbar)}>
-                    About
+                    Services
                   </Link>
                 </li>
-                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                <li className="pb-6 text-xl text-orange-600 py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
                   <Link href="#blog" onClick={() => setNavbar(!navbar)}>
-                    Blogs
+                    About Us
                   </Link>
                 </li>
-                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                <li className="pb-6 text-xl text-orange-600 py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
                   <Link href="#contact" onClick={() => setNavbar(!navbar)}>
                     Contact
-                  </Link>
-                </li>
-                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
-                  <Link href="#projects" onClick={() => setNavbar(!navbar)}>
-                    Projects
                   </Link>
                 </li>
               </ul>
